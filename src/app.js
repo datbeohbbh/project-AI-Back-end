fefa7a8cfe7795a3b9a6246ebf272f510f2dfe55
datbeohbbh <phongtomfapp@gmail.com>
2021-10-06 23:06:12 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-express = require('express');
-morgan = require('morgan');
-bodyParser = require('body-parser');
-config = require('./config');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const path = require('path');
 
 upload_router = require('./routes/upload_router');
 emotion_recognition_router = require('./routes/emotion_recognition_router');
+image_router = require('./routes/image_route');
 
 const app = express();
 
@@ -12,7 +14,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
-app.use(express.static(__dirname + '/assets/public'));
+app.use(express.static(path.join(__dirname,'/assets/public')));
+app.use(express.static(path.join(__dirname,'/routes')));
 
 app.get('/',(request,response) => {
     response.redirect('index.html');
@@ -20,6 +23,7 @@ app.get('/',(request,response) => {
 
 app.use('/uploads',upload_router);
 app.use('/emotions',emotion_recognition_router);
+app.use('/images',image_router);
 
 app.listen(config.port,config.host,() => {
     console.log(`Server running at http://${config.host}:${config.port}/`);
